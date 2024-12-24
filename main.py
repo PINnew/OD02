82dcd64f2ff2a4609e07d13d9bc1de1fe797888a
PINnew <pinnew@ya.ru>
2024-12-24 17:41:48 +0400

    add main

---
+++ main.py
@@ -0,0 +1,32 @@
+class Stack:
+   def __init__(self):
+       self.items = []
+
+# is_empty — проверка, пустой ли стек
+   def is_empty(self):
+       return self.items == []
+
+# push — добавление элемента в стек
+   def push(self, item):
+       self.items.append(item)
+
+# pop — удаление верхнего элемента
+   def pop(self):
+       return self.items.pop()
+
+# peek — получение верхнего элемента без удаления
+   def peek(self):
+       return self.items[-1]
+
+
+stack = Stack()
+
+print(stack.is_empty()) # проверка stack он пустой значит True
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.is_empty()) # проверка stack он пустой значит False
+
+print(stack.peek()) # проверка последнего stack 3
